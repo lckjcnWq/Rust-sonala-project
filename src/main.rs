fba1e13916dc19a922460f47a02bd99604f023fd
lckjcnWq <17324432872@163.com>
2025-11-05 09:23:55 +0800

    update:add init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, world!");
+
+    let a = 10;
+    let b: i32 = 20;
+    let mut c = 30;
+    let d = 30_i32;
+    let e = add(1, 2);
+    println!("{}", e);
+
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
 }
